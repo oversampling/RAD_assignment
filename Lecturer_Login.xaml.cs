420f8f54f6a7088ec703910231540e72a6c4f343
chan1992241 <chan1992241@gmail.com>
2022-08-21 03:10:15 +0800

    Features: Lecturer Login

--- Lecturer_Login.xaml.cs
+++ Lecturer_Login.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Google.Cloud.Firestore;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +24,11 @@ namespace RAD_assignment
     /// </summary>
     public sealed partial class Lecturer_Login : Page
     {
+        private FirestoreDb db;
         public Lecturer_Login()
         {
             this.InitializeComponent();
+            db = FirestoreDb.Create("booknow-61e27");
         }
         private void adminPageLogin_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
         {
@@ -41,9 +45,36 @@ namespace RAD_assignment
             this.Frame.Navigate(typeof(Lecturer_Login));
         }
 
-        private void btn_login_Click(object sender, RoutedEventArgs e)
+        private async void btn_login_Click(object sender, RoutedEventArgs e)
         {
+            Query qref = db.Collection("Lecturer").WhereEqualTo("username", txb_username.Text).WhereEqualTo("password", txb_password.Text).WhereEqualTo("university", txb_university.Text);
+            QuerySnapshot capitalQuerySnapshot = await qref.GetSnapshotAsync();
 
+            if (capitalQuerySnapshot.Count > 0)
+            {
+                foreach (DocumentSnapshot documentSnapshot in capitalQuerySnapshot.Documents)
+                {
+                    if (documentSnapshot.Exists)
+                    {
+                        string documentID = documentSnapshot.Id;
+                        Dictionary<string, object> data = documentSnapshot.ToDictionary();
+                        foreach (KeyValuePair<string, object> pair in data)
+                        {
+                            Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+                        }
+                        Dictionary<string, string> loginId = new Dictionary<string, string>();
+                        loginId.Add("studentId", documentID);
+                        Frame.Navigate(typeof(Lecturer_Main_Page), loginId);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                new ToastContentBuilder()
+                    .AddText("Wrong Username Or Password", hintMaxLines: 1)
+                    .AddText("Please reenter your username and password").Show();
+            }
         }
     }
 }
